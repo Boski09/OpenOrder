5ddd4e5a1f0cf21e9617fcbbb17fab6811dcd31a
sadeelanjum <sadeel094@gmail.com>
2021-11-03 02:01:38 +0500

    Update main.tf
    OpenOrder APIs

--- api/main.tf
+++ api/main.tf
@@ -98,6 +98,188 @@ resource "aws_api_gateway_integration_response" "api_mthd_02" {
   status_code = aws_api_gateway_method_response.api_mthd_02.status_code
 }
 
+
+#/api/v1/getorderlist
+resource "aws_api_gateway_resource" "api_rsc_04" {
+  parent_id   = aws_api_gateway_resource.api_rsc_02.id
+  path_part   = "getorderlist"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+#/api/v1/getorderlist/OPTIONS
+resource "aws_api_gateway_method" "api_mthd_03" {
+  authorization = "NONE"
+  http_method   = "OPTIONS"
+  resource_id   = aws_api_gateway_resource.api_rsc_04.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+resource "aws_api_gateway_integration" "api_mthd_03" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.api_rsc_04.id
+  http_method             = aws_api_gateway_method.api_mthd_03.http_method
+  timeout_milliseconds    = 29000
+  type                    = "MOCK"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+}
+resource "aws_api_gateway_method_response" "api_mthd_03" {
+  rest_api_id         = aws_api_gateway_rest_api.api.id
+  resource_id         = aws_api_gateway_resource.api_rsc_04.id
+  http_method         = aws_api_gateway_method.api_mthd_03.http_method
+  status_code         = "200"
+  response_models     = {"application/json"="Empty"}
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+resource "aws_api_gateway_integration_response" "api_mthd_03" {
+  rest_api_id         = aws_api_gateway_rest_api.api.id
+  resource_id         = aws_api_gateway_resource.api_rsc_04.id
+  http_method         = aws_api_gateway_method.api_mthd_03.http_method
+  status_code         = aws_api_gateway_method_response.api_mthd_03.status_code
+  response_parameters = { 
+    "method.response.header.Access-Control-Allow-Headers" = "'pregma, cache-control, expires'"
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE, GET, HEAD, PATCH, PUT, POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  } 
+}
+
+#/api/v1/getorderlist/GET
+resource "aws_api_gateway_method" "api_mthd_04" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.api_rsc_04.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+resource "aws_api_gateway_integration" "api_mthd_04" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.api_rsc_04.id
+  http_method             = aws_api_gateway_method.api_mthd_04.http_method
+  type                    = "AWS_PROXY"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+  integration_http_method = "POST"
+  uri                     = var.lambda_invoke_arn
+  timeout_milliseconds    = 29000
+
+}
+resource "aws_api_gateway_method_response" "api_mthd_04" {
+  rest_api_id     = aws_api_gateway_rest_api.api.id
+  resource_id     = aws_api_gateway_resource.api_rsc_04.id
+  http_method     = aws_api_gateway_method.api_mthd_04.http_method
+  status_code     = "200"
+  response_models = {"application/json"="Empty"}
+}
+resource "aws_api_gateway_integration_response" "api_mthd_04" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.api_rsc_04.id
+  http_method = aws_api_gateway_method.api_mthd_04.http_method
+  status_code = aws_api_gateway_method_response.api_mthd_04.status_code
+  # response_parameters = { 
+  #   "method.response.header.Access-Control-Allow-Headers" = "'pregma, cache-control, expires'"
+  #   "method.response.header.Access-Control-Allow-Methods" = "'DELETE, GET, HEAD, PATCH, PUT, POST,OPTIONS'",
+  #   "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  # } 
+#   response_parameters_in_json = <<PARAMS
+# {
+#     "method.response.header.Access-Control-Allow-Origin": "'*'"
+# }
+# PARAMS
+  response_templates = {
+    "application/json" = <<EOF
+{
+  "a":"b",
+  "c":"d",
+  "e":"f"
+}
+EOF
+  }
+
+}
+
+
+#/api/v1/orderlist
+resource "aws_api_gateway_resource" "api_rsc_05" {
+  parent_id   = aws_api_gateway_resource.api_rsc_02.id
+  path_part   = "orderlist"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+
+#/api/v1/getorderlist/GET
+resource "aws_api_gateway_method" "api_mthd_05" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.api_rsc_05.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+resource "aws_api_gateway_integration" "api_mthd_05" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.api_rsc_05.id
+  http_method             = aws_api_gateway_method.api_mthd_05.http_method
+  type                    = "AWS_PROXY"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+  integration_http_method = "POST"
+  uri                     = var.lambda_invoke_arn
+  timeout_milliseconds    = 29000
+
+}
+resource "aws_api_gateway_method_response" "api_mthd_05" {
+  rest_api_id     = aws_api_gateway_rest_api.api.id
+  resource_id     = aws_api_gateway_resource.api_rsc_05.id
+  http_method     = aws_api_gateway_method.api_mthd_05.http_method
+  status_code     = "200"
+  response_models = {"application/json"="Empty"}
+}
+resource "aws_api_gateway_integration_response" "api_mthd_05" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.api_rsc_05.id
+  http_method = aws_api_gateway_method.api_mthd_05.http_method
+  status_code = aws_api_gateway_method_response.api_mthd_05.status_code
+}
+
+
+#/api/v1/orderlist/OPTIONS
+resource "aws_api_gateway_method" "api_mthd_06" {
+  authorization = "NONE"
+  http_method   = "OPTIONS"
+  resource_id   = aws_api_gateway_resource.api_rsc_05.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+resource "aws_api_gateway_integration" "api_mthd_06" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.api_rsc_05.id
+  http_method             = aws_api_gateway_method.api_mthd_06.http_method
+  timeout_milliseconds    = 29000
+  type                    = "MOCK"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+}
+resource "aws_api_gateway_method_response" "api_mthd_06" {
+  rest_api_id         = aws_api_gateway_rest_api.api.id
+  resource_id         = aws_api_gateway_resource.api_rsc_05.id
+  http_method         = aws_api_gateway_method.api_mthd_06.http_method
+  status_code         = "200"
+  response_models     = {"application/json"="Empty"}
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+resource "aws_api_gateway_integration_response" "api_mthd_06" {
+  rest_api_id         = aws_api_gateway_rest_api.api.id
+  resource_id         = aws_api_gateway_resource.api_rsc_05.id
+  http_method         = aws_api_gateway_method.api_mthd_06.http_method
+  status_code         = aws_api_gateway_method_response.api_mthd_06.status_code
+  response_parameters = { 
+    "method.response.header.Access-Control-Allow-Headers" = "'pregma, cache-control, expires'"
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE, GET, HEAD, PATCH, PUT, POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
+
+
 #Lambda permission
 resource "aws_lambda_permission" "lambda_permission" {
   statement_id  = "AllowExecutionFromApi"
@@ -108,10 +290,10 @@ resource "aws_lambda_permission" "lambda_permission" {
 }
 
 #API deployment
-resource "aws_api_gateway_deployment" "api_deployment" {
-  depends_on        = [aws_api_gateway_method.api_mthd_02]
-  rest_api_id       = aws_api_gateway_rest_api.api.id
-  description       = "Deployed at ${timestamp()}"
-  stage_name        = var.stage_name
-  stage_description = "Deployed at ${timestamp()}"
-}
+# resource "aws_api_gateway_deployment" "api_deployment" {
+#   depends_on        = [aws_api_gateway_method.api_mthd_02]
+#   rest_api_id       = aws_api_gateway_rest_api.api.id
+#   description       = "Deployed at ${timestamp()}"
+#   stage_name        = var.stage_name
+#   stage_description = "Deployed at ${timestamp()}"
+# }
